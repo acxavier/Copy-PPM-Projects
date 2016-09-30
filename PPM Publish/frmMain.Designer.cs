b98dc3ff5f964e52be4adf299869d0ddaf9a46bc
Andre Xavier <acxavier@gmail.com>
2016-09-30 17:33:44 -0300

    Add project files.

---
+++ PPM Publish/frmMain.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace PPM_Publish_Deploy
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblPWAUrlDestino = new System.Windows.Forms.Label();
+            this.txtPWAUrlDestino = new System.Windows.Forms.TextBox();
+            this.lblSenha = new System.Windows.Forms.Label();
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.btnCarregar = new System.Windows.Forms.Button();
+            this.txtPwdPPM = new System.Windows.Forms.TextBox();
+            this.txtUsuarioPPM = new System.Windows.Forms.TextBox();
+            this.lblUsuarioPPM = new System.Windows.Forms.Label();
+            this.lblSenhaPPM = new System.Windows.Forms.Label();
+            this.gbxPPMExpress = new System.Windows.Forms.GroupBox();
+            this.txtPWAUrlOrigem = new System.Windows.Forms.TextBox();
+            this.lblPWAUrlOrigem = new System.Windows.Forms.Label();
+            this.gbxDestino = new System.Windows.Forms.GroupBox();
+            this.txtProjeto = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.gbxPPMExpress.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblPWAUrlDestino
+            // 
+            this.lblPWAUrlDestino.AutoSize = true;
+            this.lblPWAUrlDestino.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPWAUrlDestino.Location = new System.Drawing.Point(31, 272);
+            this.lblPWAUrlDestino.Name = "lblPWAUrlDestino";
+            this.lblPWAUrlDestino.Size = new System.Drawing.Size(109, 29);
+            this.lblPWAUrlDestino.TabIndex = 0;
+            this.lblPWAUrlDestino.Text = "PWA Url:";
+            // 
+            // txtPWAUrlDestino
+            // 
+            this.txtPWAUrlDestino.Location = new System.Drawing.Point(146, 275);
+            this.txtPWAUrlDestino.Name = "txtPWAUrlDestino";
+            this.txtPWAUrlDestino.Size = new System.Drawing.Size(492, 26);
+            this.txtPWAUrlDestino.TabIndex = 1;
+            this.txtPWAUrlDestino.Text = "https://senacminas.sharepoint.com/sites/pwa/";
+            // 
+            // lblSenha
+            // 
+            this.lblSenha.AutoSize = true;
+            this.lblSenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSenha.Location = new System.Drawing.Point(52, 381);
+            this.lblSenha.Name = "lblSenha";
+            this.lblSenha.Size = new System.Drawing.Size(88, 29);
+            this.lblSenha.TabIndex = 2;
+            this.lblSenha.Text = "Senha:";
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsuario.Location = new System.Drawing.Point(38, 325);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(102, 29);
+            this.lblUsuario.TabIndex = 3;
+            this.lblUsuario.Text = "Usuário:";
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(146, 329);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(492, 26);
+            this.txtUsuario.TabIndex = 4;
+            this.txtUsuario.Text = "pwa.admin@mg.senac.br";
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Location = new System.Drawing.Point(146, 385);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.PasswordChar = '*';
+            this.txtSenha.Size = new System.Drawing.Size(379, 26);
+            this.txtSenha.TabIndex = 5;
+            this.txtSenha.Text = "FDAR93!E@";
+            // 
+            // btnCarregar
+            // 
+            this.btnCarregar.Location = new System.Drawing.Point(571, 459);
+            this.btnCarregar.Name = "btnCarregar";
+            this.btnCarregar.Size = new System.Drawing.Size(93, 40);
+            this.btnCarregar.TabIndex = 6;
+            this.btnCarregar.Text = "Carregar";
+            this.btnCarregar.UseVisualStyleBackColor = true;
+            this.btnCarregar.Click += new System.EventHandler(this.btnCarregar_Click);
+            // 
+            // txtPwdPPM
+            // 
+            this.txtPwdPPM.Location = new System.Drawing.Point(127, 157);
+            this.txtPwdPPM.Name = "txtPwdPPM";
+            this.txtPwdPPM.PasswordChar = '*';
+            this.txtPwdPPM.Size = new System.Drawing.Size(379, 26);
+            this.txtPwdPPM.TabIndex = 10;
+            this.txtPwdPPM.Text = "PRDSP@789";
+            // 
+            // txtUsuarioPPM
+            // 
+            this.txtUsuarioPPM.Location = new System.Drawing.Point(127, 101);
+            this.txtUsuarioPPM.Name = "txtUsuarioPPM";
+            this.txtUsuarioPPM.Size = new System.Drawing.Size(492, 26);
+            this.txtUsuarioPPM.TabIndex = 9;
+            this.txtUsuarioPPM.Text = "sp_admin_prd";
+            // 
+            // lblUsuarioPPM
+            // 
+            this.lblUsuarioPPM.AutoSize = true;
+            this.lblUsuarioPPM.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsuarioPPM.Location = new System.Drawing.Point(19, 97);
+            this.lblUsuarioPPM.Name = "lblUsuarioPPM";
+            this.lblUsuarioPPM.Size = new System.Drawing.Size(102, 29);
+            this.lblUsuarioPPM.TabIndex = 8;
+            this.lblUsuarioPPM.Text = "Usuário:";
+            // 
+            // lblSenhaPPM
+            // 
+            this.lblSenhaPPM.AutoSize = true;
+            this.lblSenhaPPM.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSenhaPPM.Location = new System.Drawing.Point(33, 153);
+            this.lblSenhaPPM.Name = "lblSenhaPPM";
+            this.lblSenhaPPM.Size = new System.Drawing.Size(88, 29);
+            this.lblSenhaPPM.TabIndex = 7;
+            this.lblSenhaPPM.Text = "Senha:";
+            // 
+            // gbxPPMExpress
+            // 
+            this.gbxPPMExpress.Controls.Add(this.txtPWAUrlOrigem);
+            this.gbxPPMExpress.Controls.Add(this.txtPwdPPM);
+            this.gbxPPMExpress.Controls.Add(this.lblPWAUrlOrigem);
+            this.gbxPPMExpress.Controls.Add(this.txtUsuarioPPM);
+            this.gbxPPMExpress.Controls.Add(this.lblUsuarioPPM);
+            this.gbxPPMExpress.Controls.Add(this.lblSenhaPPM);
+            this.gbxPPMExpress.Location = new System.Drawing.Point(19, 20);
+            this.gbxPPMExpress.Name = "gbxPPMExpress";
+            this.gbxPPMExpress.Size = new System.Drawing.Size(637, 206);
+            this.gbxPPMExpress.TabIndex = 11;
+            this.gbxPPMExpress.TabStop = false;
+            this.gbxPPMExpress.Text = "PPM Express";
+            // 
+            // txtPWAUrlOrigem
+            // 
+            this.txtPWAUrlOrigem.Location = new System.Drawing.Point(127, 44);
+            this.txtPWAUrlOrigem.Name = "txtPWAUrlOrigem";
+            this.txtPWAUrlOrigem.Size = new System.Drawing.Size(492, 26);
+            this.txtPWAUrlOrigem.TabIndex = 14;
+            this.txtPWAUrlOrigem.Text = "http://armg83/PWA/";
+            // 
+            // lblPWAUrlOrigem
+            // 
+            this.lblPWAUrlOrigem.AutoSize = true;
+            this.lblPWAUrlOrigem.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPWAUrlOrigem.Location = new System.Drawing.Point(12, 41);
+            this.lblPWAUrlOrigem.Name = "lblPWAUrlOrigem";
+            this.lblPWAUrlOrigem.Size = new System.Drawing.Size(109, 29);
+            this.lblPWAUrlOrigem.TabIndex = 13;
+            this.lblPWAUrlOrigem.Text = "PWA Url:";
+            // 
+            // gbxDestino
+            // 
+            this.gbxDestino.Location = new System.Drawing.Point(19, 242);
+            this.gbxDestino.Name = "gbxDestino";
+            this.gbxDestino.Size = new System.Drawing.Size(642, 200);
+            this.gbxDestino.TabIndex = 12;
+            this.gbxDestino.TabStop = false;
+            this.gbxDestino.Text = "PWA de destino";
+            // 
+            // txtProjeto
+            // 
+            this.txtProjeto.Location = new System.Drawing.Point(139, 466);
+            this.txtProjeto.Name = "txtProjeto";
+            this.txtProjeto.Size = new System.Drawing.Size(426, 26);
+            this.txtProjeto.TabIndex = 14;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(31, 462);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 29);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Projeto:";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(676, 517);
+            this.Controls.Add(this.txtProjeto);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.gbxPPMExpress);
+            this.Controls.Add(this.btnCarregar);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.lblUsuario);
+            this.Controls.Add(this.lblSenha);
+            this.Controls.Add(this.txtPWAUrlDestino);
+            this.Controls.Add(this.lblPWAUrlDestino);
+            this.Controls.Add(this.gbxDestino);
+            this.Name = "frmMain";
+            this.Text = "PPM Express";
+            this.gbxPPMExpress.ResumeLayout(false);
+            this.gbxPPMExpress.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPWAUrlDestino;
+        private System.Windows.Forms.TextBox txtPWAUrlDestino;
+        private System.Windows.Forms.Label lblSenha;
+        private System.Windows.Forms.Label lblUsuario;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.Button btnCarregar;
+        private System.Windows.Forms.TextBox txtPwdPPM;
+        private System.Windows.Forms.TextBox txtUsuarioPPM;
+        private System.Windows.Forms.Label lblSenhaPPM;
+        private System.Windows.Forms.GroupBox gbxPPMExpress;
+        private System.Windows.Forms.GroupBox gbxDestino;
+        private System.Windows.Forms.Label lblUsuarioPPM;
+        private System.Windows.Forms.TextBox txtPWAUrlOrigem;
+        private System.Windows.Forms.Label lblPWAUrlOrigem;
+        private System.Windows.Forms.TextBox txtProjeto;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ PPM Publish/frmMain.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.ProjectServer.Client;
+using Microsoft.SharePoint.Client;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace PPM_Publish_Deploy
+{
+
+    public partial class frmMain : System.Windows.Forms.Form
+    {
+        private static ProjectContext ppmContext;
+        private static ProjectContext newppmContext;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCarregar_Click(object sender, EventArgs e)
+        {
+            // Conectando no PPM Express
+            ppmContext = new ProjectContext(txtPWAUrlOrigem.Text);
+            
+            var secpass = new SecureString();
+            string pass = txtPwdPPM.Text;
+            pass.ToList().ForEach(secpass.AppendChar);
+
+            ppmContext.Credentials = new NetworkCredential("sp_admin_prd", secpass, "SENAC_ADM");
+
+            ppmContext.Load(ppmContext.Projects);
+            ppmContext.ExecuteQuery();
+
+            // Conectando no novo ambiente
+            newppmContext = new ProjectContext(txtPWAUrlDestino.Text);
+
+            secpass = new SecureString();
+            pass = txtSenha.Text;
+            pass.ToList().ForEach(secpass.AppendChar);
+
+            newppmContext.Credentials = new SharePointOnlineCredentials(txtUsuario.Text, secpass);
+
+            // Carregando projetos
+            newppmContext.Load(newppmContext.Projects);
+            newppmContext.ExecuteQuery();
+
+            // Carregando projetos
+            newppmContext.Load(newppmContext.CustomFields);
+            newppmContext.ExecuteQuery();
+
+
+            var prjOrigemComCustomFields = ppmContext.Projects.FirstOrDefault(c => c.Name == txtProjeto.Text).IncludeCustomFields;
+            ppmContext.Load(prjOrigemComCustomFields);
+            ppmContext.ExecuteQuery();
+
+            //foreach (var prjOrigem in ppmContext.Projects)
+            //{
+            //}
+            //var prjOrigemComCustomFields = prjOrigem.IncludeCustomFields;
+            //ppmContext.Load(prjOrigemComCustomFields);
+            //ppmContext.ExecuteQuery();
+            try
+            {
+                var prjDestinoComCustomFields = newppmContext.Projects.FirstOrDefault(c => c.Name == prjOrigemComCustomFields.Name).CheckOut().IncludeCustomFields;
+                newppmContext.Load(prjDestinoComCustomFields);
+                newppmContext.ExecuteQuery();
+
+                foreach (var cfOrigem in prjOrigemComCustomFields.FieldValues)
+                {
+                    var cfDetalhe = newppmContext.CustomFields.FirstOrDefault(c => c.InternalName == cfOrigem.Key);
+                    if (cfDetalhe.Formula == null)
+                    {
+                        prjDestinoComCustomFields.SetCustomFieldValue(cfOrigem.Key, cfOrigem.Value);
+                    }
+
+
+                }
+
+                prjDestinoComCustomFields.Update();
+                var queueJob = prjDestinoComCustomFields.Publish(true);
+                newppmContext.WaitForQueue(queueJob, 20);
+
+                var pubProject = newppmContext.Projects.GetByGuid(prjDestinoComCustomFields.Id);
+                pubProject.SubmitToWorkflow();
+                newppmContext.ExecuteQuery();
+
+
+            }
+            catch (Exception ex)
+            {
+                        
+
+            }
+
+            MessageBox.Show("Dados do projeto copiado com sucesso!");
+
+
+
+        }
+    }
+}
